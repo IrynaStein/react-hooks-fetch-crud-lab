b24c29f2c86782d2f38fc449a717fc589595206f
Iryna Stein <isophiestein@gmail.com>
2021-06-21 18:32:00 -0600

    Completed Lab

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,8 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, deleteQuestion, answerChanged }) {
   const { id, prompt, answers, correctIndex } = question;
+
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -9,15 +10,34 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleClick() {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE"
+    })
+    deleteQuestion(question.id)
+  }
+
+  function handleChange(e) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        "correctIndex": parseInt(e.target.value)
+      })
+    })
+        .then(resp => resp.json())
+        .then(data => answerChanged(data))
+    }
+  
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select onChange={handleChange} defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleClick}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem"
+
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([])
+
+  const URL = "http://localhost:4000/questions"
+
+  useEffect(() => {
+    fetch(URL)
+      .then(resp => resp.json())
+      .then(data => setQuestions(data))
+  }, [])
+
+
+  function deleteQuestion(id) {
+    const updatedQuestions = questions.filter((question) => question.id !== id)
+    setQuestions(updatedQuestions)
+  }
+
+  function answerChanged(data) {
+    console.log(data)
+    const newAnswerChange = questions.map((question) => {
+      if (question.id === data.id){
+        return data
+      }
+      else {
+        return question
+      }
+    })
+    setQuestions(newAnswerChange)
+  }
+
+  console.log(questions)
+
+  const getAQuestion = () => questions.map((question) => {
+    return (
+      <QuestionItem
+        key={question.id}
+        question={question}
+        deleteQuestion={deleteQuestion}
+        answerChanged={answerChanged}
+      />
+    )
+  })
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {getAQuestion()}
+      </ul>
     </section>
   );
 }
